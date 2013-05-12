186e5c60d551b869ac951bb9d76c6bc064aaf296
joostdekeijzer <joost@dekeijzer.org>
2013-05-12 23:32:05 +0200

    improved login link hanlding, dutch localisation

--- OAuth2Client.i18n.php
+++ OAuth2Client.i18n.php
@@ -16,6 +16,7 @@ $messages['en'] = array(
 
 	'oauth2client-act-as-a-client-to-any-oauth2-server' => 'Act as a client to any OAuth2 server',
 	'oauth2client-couldnotconnect' => 'Could not connect to OAuth2 Server',
+	'oauth2client-header-link-text' => '$1 login',
 
 	'oauth2client-login-header' => '$1 login',
 	'oauth2client-you-can-login-to-this-wiki-with-oauth2' => 'You can login this wiki with your $1 account',
@@ -27,8 +28,25 @@ $messages['en'] = array(
 	'oauth2client-login-with-oauth2-again' => '[[$1|You can login again with your $2 account]]',
 );
 
+$messages['nl'] = array(
+	'oauth2client' => 'OAuth2 Client',
+
+	'oauth2client-act-as-a-client-to-any-oauth2-server' => 'Act as a client to any OAuth2 server',
+	'oauth2client-couldnotconnect' => 'Kon geen verbinding maken met de OAuth2 server',
+	'oauth2client-header-link-text' => 'Aanmelden via $1',
+
+	'oauth2client-login-header' => 'Aanmelden via $1',
+	'oauth2client-you-can-login-to-this-wiki-with-oauth2' => 'U kunt zich aanmelden op deze wiki via uw $1 account',
+	'oauth2client-login-with-oauth2' => '[[$1|Aanmelden via $2]]',
+	'oauth2client-youre-already-loggedin' => "U bent al ingelogd.",
+
+	'oauth2client-logout-header' => '$1 uitgelogd',
+	'oauth2client-logged-out' => 'U bent succesvol uitgelogd.',
+	'oauth2client-login-with-oauth2-again' => '[[$1|U kunt u weer aanmelden via uw $2 account]]',
+);
+
 /**
- * Message Documentation
+ * Message Documentation, incomplete
  */
 $messages['qqq'] = array(
 	'oauth2client' => 'Link of the special page',
--- OAuth2Client.php
+++ OAuth2Client.php
@@ -64,9 +64,16 @@ class OAuth2ClientHooks {
 		# see SkinTemplate->buildPersonalUrls()
 		$page = Title::newFromURL( $wgRequest->getVal( 'title', '' ) );
 
+		$sevice_name = isset( $wgOAuth2Client['configuration']['sevice_name'] ) && 0 < strlen( $wgOAuth2Client['configuration']['sevice_name'] ) ? $wgOAuth2Client['configuration']['sevice_name'] : 'OAuth2';
+		if( isset( $wgOAuth2Client['configuration']['sevice_login_link_text'] ) && 0 < strlen( $wgOAuth2Client['configuration']['sevice_login_link_text'] ) ) {
+			$sevice_login_link_text = $wgOAuth2Client['configuration']['sevice_login_link_text'];
+		} else {
+			$sevice_login_link_text = wfMsg('oauth2client-header-link-text', $sevice_name);
+		}
+
 		$inExt = ('OAuth2Client' == substr( $page->mUrlform, 0, 12) );
 		$personal_urls['anon_oauth_login'] = array(
-			'text' => ( isset( $wgOAuth2Client['configuration']['sevice_login_link_text'] ) && 0 < strlen( $wgOAuth2Client['configuration']['sevice_login_link_text'] ) ? $wgOAuth2Client['configuration']['sevice_login_link_text'] : wfMsg('oauth2client-login-with-oauth2') ),
+			'text' => $sevice_login_link_text,
 			//'class' => ,
 			'active' => false,
 		);
